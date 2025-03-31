35ec4aa30d19e4475c9344605cfbdf0a10ab3aa9
Anton Karpov <antony.karpov@icloud.com>
2025-03-31 22:52:31 +0300

    Add tests for MetricReporter

---
+++ internal/infrastructure/metricreporter/metric_reporter_test.go
@@ -0,0 +1,30 @@
+package metricreporter
+
+import (
+	"github.com/angryscorp/alert-metrics/internal/domain"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+type MockRoundTripper struct {
+	lastRequest *http.Request
+}
+
+func (m *MockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	m.lastRequest = req
+	return &http.Response{StatusCode: http.StatusOK}, nil
+}
+
+func Test_HTTPMetricReporter_Report(t *testing.T) {
+	// Arrange
+	transport := &MockRoundTripper{}
+	mockClient := &http.Client{Transport: transport}
+	reporter := NewHTTPMetricReporter("http://example.com", mockClient)
+
+	// Act
+	reporter.Report(domain.MetricTypeGauge, "key", "value")
+
+	// Assert
+	assert.Equal(t, transport.lastRequest.URL.String(), "http://example.com/update/gauge/key/value")
+}
