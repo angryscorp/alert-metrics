74bf7a5d4db9be44b943a81244f23b3e8cfec98d
Anton Karpov <antony.karpov@icloud.com>
2025-03-31 21:38:15 +0300

    Add Worker

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"github.com/angryscorp/alert-metrics/internal/infrastructure/metricmonitor"
+	"github.com/angryscorp/alert-metrics/internal/infrastructure/metricreporter"
+	"github.com/angryscorp/alert-metrics/internal/infrastructure/metricworker"
+	"net/http"
+	"time"
+)
+
+func main() {
+	rm := metricmonitor.NewRuntimeMonitor(2 * time.Second)
+	rm.Start()
+
+	mr := metricreporter.NewHTTPMetricReporter("http://localhost:8080", &http.Client{})
+
+	worker := metricworker.NewMetricWorker(rm, mr, 10*time.Second)
+	worker.Start()
+
+	select {}
+}
---
+++ internal/infrastructure/metricworker/metric_worker.go
@@ -0,0 +1,52 @@
+package metricworker
+
+import (
+	"github.com/angryscorp/alert-metrics/internal/domain"
+	"strconv"
+	"time"
+)
+
+type MetricWorker struct {
+	metricMonitor  domain.MetricMonitor
+	metricReporter domain.MetricReporter
+	reportInterval time.Duration
+	isRunning      bool
+}
+
+func NewMetricWorker(metricMonitor domain.MetricMonitor, metricReporter domain.MetricReporter, reportInterval time.Duration) *MetricWorker {
+	return &MetricWorker{
+		metricMonitor:  metricMonitor,
+		metricReporter: metricReporter,
+		reportInterval: reportInterval,
+	}
+}
+
+func (mw *MetricWorker) Start() {
+	mw.isRunning = true
+	go mw.sendCurrentMetrics()
+}
+
+func (mw *MetricWorker) sendCurrentMetrics() {
+
+	// Send Gauge metrics
+	for key, value := range mw.metricMonitor.GetGauges() {
+		formattedValue := strconv.FormatFloat(value, 'f', -1, 64)
+		mw.metricReporter.Report(domain.MetricTypeGauge, key, formattedValue)
+	}
+
+	// Send Counter metrics
+	for key, value := range mw.metricMonitor.GetCounters() {
+		formattedValue := strconv.FormatInt(value, 10)
+		mw.metricReporter.Report(domain.MetricTypeCounter, key, formattedValue)
+	}
+
+	// Report interval
+	if mw.isRunning {
+		time.Sleep(mw.reportInterval)
+		go mw.sendCurrentMetrics()
+	}
+}
+
+func (mw *MetricWorker) Stop() {
+	mw.isRunning = false
+}
