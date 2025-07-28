6ad8ef4fd8d36cae99a89dd0ebdbf2bdaf2d7c6c
Anton Karpov <antony.karpov@icloud.com>
2025-07-28 21:25:07 +0200

    Add more tests

---
+++ internal/config/agent/config_test.go
@@ -0,0 +1,46 @@
+package agent
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewConfig(t *testing.T) {
+	t.Run("environment variables override", func(t *testing.T) {
+		// Reset flag package
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+		envVars := map[string]string{
+			"ADDRESS":         "example.com:9090",
+			"POLL_INTERVAL":   "5",
+			"REPORT_INTERVAL": "30",
+			"KEY":             "secret123",
+			"RATE_LIMIT":      "20",
+		}
+
+		expected := Config{
+			Address:                 "example.com:9090",
+			PollIntervalInSeconds:   5,
+			ReportIntervalInSeconds: 30,
+			HashKey:                 "secret123",
+			RateLimit:               20,
+		}
+
+		for key, value := range envVars {
+			_ = os.Setenv(key, value)
+		}
+
+		oldArgs := os.Args
+		os.Args = []string{"test"}
+		defer func() { os.Args = oldArgs }()
+
+		config, err := NewConfig()
+
+		require.NoError(t, err)
+		assert.Equal(t, expected, config)
+	})
+}
